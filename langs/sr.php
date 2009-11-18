ca83c42a7817cd4fc764ed45ae0131fb86fe0dae
Max Pozdeev <maxpozdeev@gmail.com>
2009-11-18 18:22:05 +0300

    language updated: Serbian (sr)

--- langs/sr.php
+++ langs/sr.php
@@ -1,8 +1,11 @@
 <?php
 
 /*
-	myTinyTodo language pack (Serbian) v1.2.x
+	myTinyTodo language pack
+	Language: Serbian
 	Author: Goran Trajkovic
+	AppVersion: v1.3.0
+	Date: 2009-11-17
 */
 
 class Lang extends DefaultLang
@@ -21,18 +24,24 @@ class Lang extends DefaultLang
 		'invalidpass' => "Неисправна лозинка?",
 		'readonly' => "само-за-читање",
 		'tagfilter' => "Категорија: ",
+		'addList' => "Направи нову листу",
+		'renameList' => "Преименуј листу",
+		'deleteList' => "Ова акција ће обрисати текућу листу са свим припадајућим пословима.\\nДа ли сте сигурни?",
+		'settingsSaved' => "Промене у подешавањима су сачуване. Поновно учитавање у току...",
 	);
 
 	var $inc = array
 	(
 		'My Tiny Todolist' => "РОКОВНИК",
-		'tab_newtask' => "нови задатак",
-		'tab_search' => "претрага",
+		'htab_newtask' => "Нови задатак",
+		'htab_search' => "Претрага",
 		'btn_add' => "Упиши",
 		'btn_search' => "Тражи",
+		'advanced_add' => "Напредно додавање (*)",
 		'searching' => "Претраживање у току...",
 		'tasks' => "Текући",
 		'edit_task' => "Измена задатка",
+		'add_task' => "Нови задатак",
 		'priority' => "Приоритет",
 		'task' => "Наслов",
 		'note' => "Опис",
@@ -53,8 +62,6 @@ class Lang extends DefaultLang
 		'months_short' => array("Jан","Феб","Maр","Aпр","Maј","Jун","Jул","Aвг","Сеп","Oкт","Нов","Дец"),
 		'months_long' => array("Јануар","Фебруар","Март","Април","Maј","Jун","Jул","Aвгуст","Септембар","Oктобар","Новембар","Децембар"),
 		'days_min' => array("Не","По","Ут","Ср","Че","Пе","Су"),
-		'date_md' => "%1\$s %2\$d",
-		'date_ymd' => "%2\$s %3\$d, %1\$d",
 		'today' => "данас",
 		'yesterday' => "јуче",
 		'tomorrow' => "сутра",
@@ -62,6 +69,19 @@ class Lang extends DefaultLang
 		'f_today' => "Данас и сутра",
 		'f_soon' => "Ускоро",
 		'tasks_and_compl' => "Сви",
+		'notes' => "Напомене:",
+		'notes_show' => "прикажи",
+		'notes_hide' => "сакриј",
+		'list_new' => "Нова листа",
+		'list_rename' => "Преименуј",
+		'list_delete' => "Обриши",
+		'alltags' => "Све категорије (тагови):",
+		'alltags_show' => "Прикажи све",
+		'alltags_hide' => "Сакриј све",
+		'a_settings' => "Подешавања",
+		'rss_feed' => "RSS Feed",
+		'feed_title' => "%s",
+		'feed_description' => "Нови задаци у групи %s",
 	);
 }
 
