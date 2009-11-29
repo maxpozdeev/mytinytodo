c9a0e3576555eee3495765556031d9fa06a59939
Max Pozdeev <maxpozdeev@gmail.com>
2009-11-29 12:18:48 +0300

    Chinese simplified (cns) language pack added

---
+++ langs/cns.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+	myTinyTodo language pack
+	Language: Simple Chinese 
+	Author: heraldboy@gmail.com
+	Url:
+	AppVersion: v1.3.1
+	Date: 2009-11-29
+*/
+
+class Lang extends DefaultLang
+{
+	var $js = array
+	(
+		'actionNote' => "内容",
+		'actionEdit' => "编辑",
+		'actionDelete' => "删除",
+		'confirmDelete' => "你确定吗?",
+		'actionNoteSave' => "保存",
+		'actionNoteCancel' => "退出",
+		'error' => "发生了错误 (点击查看详细情况)",
+		'denied' => "数据被拒绝了",
+		'invalidpass' => "密码错误",
+		'readonly' => "只读",
+		'tagfilter' => "标签:",
+		'addList' => "请输入一个新的列表名称",
+		'renameList' => "重命名列表",
+		'deleteList' => "当前列表及其包括的所有任务将被删除。\\n你确定吗?",
+		'settingsSaved' => "设置已经被保存了，正在重新引导...",
+	);
+
+	var $inc = array
+	(
+		'My Tiny Todolist' => "我的迷你任务列表",
+		'htab_newtask' => "新任务",
+		'htab_search' => "查询",
+		'btn_add' => "增加",
+		'btn_search' => "查询",
+		'advanced_add' => "高级新增",
+		'searching' => "查找",
+		'tasks' => "所有任务",
+		'taskdate_inline' => "%s 新增的",
+		'edit_task' => "编辑任务",
+		'add_task' => "新增任务",
+		'priority' => "优先级",
+		'task' => "任务",
+		'note' => "内容",
+		'save' => "保存",
+		'cancel' => "退出",
+		'password' => "密码",
+		'btn_login' => "登录",
+		'a_login' => "登录",
+		'a_logout' => "退出",
+		'tags' => "标签",
+		'tagfilter_cancel' => "取消标签过滤",
+		'sortByHand' => "手动排序",
+		'sortByPriority' => "按优先级排序",
+		'sortByDueDate' => "按到期时间排序",
+		'due' => "到期",
+		'daysago' => "%d 天前",
+		'indays' => "%d 天后",
+		'months_short' => array("1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"),
+		'months_long' => array("1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"),
+		'days_min' => array("日","一","二","三","四","五","六"),
+		'days_long' => array("星期日","星期一","星期二","星期三","星期四","星期五","星期六"),
+		'today' => "今天",
+		'yesterday' => "昨天",
+		'tomorrow' => "明天",
+		'f_past' => "过期",
+		'f_today' => "今天和明天",
+		'f_soon' => "最近的",
+		'tasks_and_compl' => "所有任务(包括完成的)",
+		'notes' => "内容:",
+		'notes_show' => "显示",
+		'notes_hide' => "隐藏",
+		'list_new' => "新增列表",
+		'list_rename' => "重命名",
+		'list_delete' => "删除",
+		'list_publish' => "公开列表",
+		'alltags' => "所有标签:",
+		'alltags_show' => "显示所有",
+		'alltags_hide' => "隐藏所有",
+		'a_settings' => "系统设置",
+		'rss_feed' => "RSS Feed",
+		'feed_title' => "%s",
+		'feed_description' => "新任务 %s",
+	);
+}
+
+?>
