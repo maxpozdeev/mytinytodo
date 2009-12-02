d943101c6c4dd985810ae1163b0db91dc1891075
Max Pozdeev <maxpozdeev@gmail.com>
2009-12-02 14:48:01 +0300

    turkish (tr) language updated

--- langs/tr.php
+++ langs/tr.php
@@ -1,38 +1,48 @@
 <?php
 
 /*
-	myTinyTodo language pack (Turkish) v1.2.x
-	Author: Uğur Okumuş (www.ugurokumus.net)
+	myTinyTodo language pack
+	Language: Turkish
+	Author: FeyyazEsat
+	Url: http://www.esttechnology.com
+	AppVersion: v1.3.1
+	Date: 2009-11-29
 */
 
 class Lang extends DefaultLang
 {
 	var $js = array
 	(
-		'actionNote' => "not ekle",
-		'actionEdit' => "düzenle",
+		'actionNote' => "not",
+		'actionEdit' => "modifiye et",
 		'actionDelete' => "sil",
-		'taskDate' => array("function(date) { return date+' tarhinden eklendi'; }"),
-		'confirmDelete' => "Emin misin?",
+		'confirmDelete' => "Eminmisiniz?",
 		'actionNoteSave' => "kaydet",
 		'actionNoteCancel' => "iptal",
-		'error' => "Hatalar var. Ayrıntılar için tıklayınız",
-		'denied' => "Erişim izniniz yok",
-		'invalidpass' => "Yanlış şifre",
+		'error' => "Bazı hatalar oluştu (detaylar için tıklayın)",
+		'denied' => "İzin verilmedi",
+		'invalidpass' => "Yanlış Şifre",
 		'readonly' => "sadece okunabilir",
 		'tagfilter' => "Etiket:",
+		'addList' => "Yeni Liste Oluştur",
+		'renameList' => "Listeyi Çıkar",
+		'deleteList' => "Mevcut liste ve içindeki tüm görevler silinecek.\\nEminmisiniz?",
+		'settingsSaved' => "Ayarlar kaydedildi. Tekrar Yükleniyor...",
 	);
 
 	var $inc = array
 	(
-		'My Tiny Todolist' => "Görev Listem",
-		'tab_newtask' => "yeni görev",
-		'tab_search' => "ara",
-		'btn_add' => "ekle",
-		'btn_search' => "ara",
-		'searching' => "ara",
+		'My Tiny Todolist' => "My Tiny Todolist",
+		'htab_newtask' => "Yeni görev",
+		'htab_search' => "Ara",
+		'btn_add' => "Ekle",
+		'btn_search' => "Ara",
+		'advanced_add' => "İleri düzey",
+		'searching' => "Aranıyor ->",
 		'tasks' => "Görevler",
-		'edit_task' => "Görevi Düzenle",
+		'taskdate_inline' => "%s a Eklendi",
+		'edit_task' => "Görev düzenle",
+		'add_task' => "Yeni görev",
 		'priority' => "Öncelik",
 		'task' => "Görev",
 		'note' => "Not",
@@ -43,25 +53,38 @@ class Lang extends DefaultLang
 		'a_login' => "Giriş",
 		'a_logout' => "Çıkış",
 		'tags' => "Etiketler",
-		'tagfilter_cancel' => "filtreyi temizle",
-		'sortByHand' => "sırala",
-		'sortByPriority' => "önceliğe göre diz",
-		'sortByDueDate' => "bitiş tarihine göre diz",
-		'due' => "Bitiş Tarihi",
-		'daysago' => "%d gün önce",
-		'indays' => "%d gün içerisinde",
-		'months_short' => array("Oca","Şub","Mar","Nis","May","Haz","Tem","Ağs","Eyl","Ekm","Kas","Ara"),
+		'tagfilter_cancel' => "Filtre iptal",
+		'sortByHand' => "Elle sırala",
+		'sortByPriority' => "Önceliğe göre sırala",
+		'sortByDueDate' => "Vade tarihine göre düzenle",
+		'due' => "Vade",
+		'daysago' => "%d Gün önce",
+		'indays' => "%d Gün içinde",
+		'months_short' => array("Oca","Şub","Mar","Nis","May","Haz","Tem","Ağu","Eyl","Eki","Kas","Ara"),
 		'months_long' => array("Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"),
-		'days_min' => array("Pz","Pt","Sa","Çr","Pr","Cu","Ct"),
-		'date_md' => "%1\$s %2\$d",
-		'date_ymd' => "%2\$s %3\$d, %1\$d",
+		'days_min' => array("Pa","Pz","Sa","Ça","Pe","Cu","Ct"),
+		'days_long' => array("Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"),
 		'today' => "bugün",
 		'yesterday' => "dün",
 		'tomorrow' => "yarın",
-		'f_past' => "zaman aşımı",
-		'f_today' => "Bugün ve yarın",
+		'f_past' => "Vade bitimi",
+		'f_today' => "Bugün ve Yarın",
 		'f_soon' => "Yakında",
-		'tasks_and_compl' => "Görev + tamamlanmış",
+		'tasks_and_compl' => "Görevler + Tamamlanan",
+		'notes' => "Notlar:",
+		'notes_show' => "Göster",
+		'notes_hide' => "Gizle",
+		'list_new' => "Yeni Liste",
+		'list_rename' => "Yeniden adlandır",
+		'list_delete' => "Sil",
+		'list_publish' => "Listeyi yayımla",
+		'alltags' => "Tüm Etiketler:",
+		'alltags_show' => "Tümünü göster",
+		'alltags_hide' => "Tümünü gizle",
+		'a_settings' => "Ayarlar",
+		'rss_feed' => "RSS Beslemesi",
+		'feed_title' => "%s",
+		'feed_description' => "Yeni görevler %s nin içinde",
 	);
 }
 
