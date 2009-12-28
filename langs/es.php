eb14a086d094e21d7ccb44541709ac025bdc0393
Max Pozdeev <maxpozdeev@gmail.com>
2009-12-28 19:15:32 +0300

    Spanish languge updated to v1.3.1 (thanks to Thanatermesis)

--- langs/es.php
+++ langs/es.php
@@ -1,66 +1,90 @@
 <?php
 
-/**
- * Spanish translation for My Tiny Todolist v1.2.5
- * Author: Diego Jiménez http://www.zasbang.com
- */
+/*
+	myTinyTodo language pack
+	Language: Español
+	Translated: Spanish
+	Author: Thanatermesis
+	AppVersion: v1.3.1
+	Date: 2009-12-24
+*/
 
 class Lang extends DefaultLang
 {
 	var $js = array
 	(
-		'actionNote' => "Notas",
-		'actionEdit' => "Modificar",
-		'actionDelete' => "Borrar",
-		'taskDate' => array("function(date) { return 'añadido el '+date; }"),
-		'confirmDelete' => "¿Estás seguro?",
-		'actionNoteSave' => "Guardar",
-		'actionNoteCancel' => "Cancelar",
-		'error' => "Ha ocurrido algún error (Click para más detalles)",
-		'denied' => "Acceso denegado",
-		'invalidpass' => "Contraseña Incorrecta",
-		'readonly' => "solo lectura",
+		'actionNote' => "nota",
+		'actionEdit' => "modificar",
+		'actionDelete' => "borrar",
+		'confirmDelete' => "Está seguro ?",
+		'actionNoteSave' => "guardar",
+		'actionNoteCancel' => "cancelar",
+		'error' => "Se ha producido un error (clicar para detalles)",
+		'denied' => "Access denied",
+		'invalidpass' => "Wrong password",
+		'readonly' => "read-only",
 		'tagfilter' => "Tag:",
+		'addList' => "Crear nueva lista",
+		'renameList' => "Renombrar lista",
+		'deleteList' => "Esto borrará su lista actual con todas sus tareas incluídas.\\nEstá seguro ?",
+		'settingsSaved' => "Settings saved. Reloading...",
 	);
 
 	var $inc = array
 	(
-		'My Tiny Todolist' => "My Tiny Todolist",
-		'tab_newtask' => "Nueva tarea",
-		'tab_search' => "Buscar",
+		'My Tiny Todolist' => "Mi lista de tareas",
+		'htab_newtask' => "Nueva tarea",
+		'htab_search' => "Buscar",
 		'btn_add' => "Añadir",
 		'btn_search' => "Buscar",
-		'searching' => "Buscando:",
+		'advanced_add' => "Avanzado",
+		'searching' => "Buscar por",
 		'tasks' => "Tareas",
-		'edit_task' => "Editar tarea",
+		'taskdate_inline' => "añadido en %s",
+		'edit_task' => "Editar Tarea",
+		'add_task' => "Nueva Tarea",
 		'priority' => "Prioridad",
 		'task' => "Tarea",
 		'note' => "Nota",
 		'save' => "Guardar",
 		'cancel' => "Cancelar",
-		'password' => "Contraseña",
+		'password' => "Password",
 		'btn_login' => "Login",
 		'a_login' => "Login",
-		'a_logout' => "Salir",
+		'a_logout' => "Logout",
 		'tags' => "Tags",
-		'tagfilter_cancel' => "Cancelar filtro",
-		'sortByHand' => "Ordenar manualmente",
-		'sortByPriority' => "Ordenar por prioridad",
+		'tagfilter_cancel' => "cancelar filtro",
+		'sortByHand' => "Ordenar por Mano",
+		'sortByPriority' => "Ordenar por Prioridad",
 		'sortByDueDate' => "Ordenar por fecha de vencimiento",
-		'due' => "Termina",
-		'daysago' => "%d días atrás",
-		'indays' => "en %d días",
+		'due' => "Vencimiento",
+		'daysago' => "hace %d días",
+		'indays' => "in %d days",
 		'months_short' => array("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"),
-		'days_min' => array("Lun","Mar","Mie","Jue","Vie","Sab","Dom"),
-		'date_md' => "%1\$s %2\$d",
-		'date_ymd' => "%2\$s %3\$d, %1\$d",
+		'months_long' => array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"),
+		'days_min' => array("Do","Lu","Ma","Mi","Ju","Vi","Sa"),
+		'days_long' => array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sabado"),
 		'today' => "hoy",
 		'yesterday' => "ayer",
 		'tomorrow' => "mañana",
-		'f_past' => "Atrasadas",
-		'f_today' => "Hoy y mañana",
+		'f_past' => "Atrasados",
+		'f_today' => "Hoy y Mañana",
 		'f_soon' => "Pronto",
-		'tasks_and_compl' => "Tareas + completadas",
+		'tasks_and_compl' => "Tareas + completados",
+		'notes' => "Notea:",
+		'notes_show' => "Mostrar",
+		'notes_hide' => "Esconder",
+		'list_new' => "Nueva Lista",
+		'list_rename' => "Renombrar",
+		'list_delete' => "Borrar",
+		'list_publish' => "Publicar la Lista",
+		'alltags' => "Todos los Tags:",
+		'alltags_show' => "Mostrar todo",
+		'alltags_hide' => "Esconder todo",
+		'a_settings' => "Configuración",
+		'rss_feed' => "RSS Feed",
+		'feed_title' => "%s",
+		'feed_description' => "New tasks in %s",
 	);
 }
 
