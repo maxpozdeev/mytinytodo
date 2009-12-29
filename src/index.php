c64c016d37117290dd6a973023160954375b59b7
Max Pozdeev <maxpozdeev@gmail.com>
2009-12-29 14:58:34 +0300

    * some js files caching

--- src/index.php
+++ src/index.php
@@ -47,8 +47,8 @@ function _e($s)
 
 <script type="text/javascript" src="jquery/jquery-1.3.2.min.js"></script>
 <script type="text/javascript" src="jquery/jquery-ui-1.7.2.custom.min.js"></script>
-<script type="text/javascript" src="ajax.lang.php"></script>
-<script type="text/javascript" src="ajax.js"></script>
+<script type="text/javascript" src="ajax.lang.php?v=@VERSION"></script>
+<script type="text/javascript" src="ajax.js?v=@VERSION"></script>
 <script type="text/javascript" src="jquery/jquery.autocomplete.min.js"></script>
 
 <script type="text/javascript">
