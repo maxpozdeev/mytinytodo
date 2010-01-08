f7663327b0ed82a26846d5787209c90ffbf7a6f0
Max Pozdeev <maxpozdeev@gmail.com>
2010-01-08 19:13:43 +0300

    added Slovenian translation (thanks to Janez Troha)

---
+++ langs/sl.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+	myTinyTodo language pack
+	Language: Slovenian
+	Original name: slovensko
+	Author: Janez Troha
+	Author Url:
+	AppVersion: v1.3.2
+	Date: 2010-01-08
+*/
+
+class Lang extends DefaultLang
+{
+	var $js = array
+	(
+		'confirmDelete' => "Ali ste prepričani da želite izbisati izbrano nalogo?",
+		'actionNoteSave' => "shrani",
+		'actionNoteCancel' => "prekliči",
+		'error' => "Napaka (klikni za podrobnosti)",
+		'denied' => "Dostop zavrnjen",
+		'invalidpass' => "Napačno geslo",
+		'tagfilter' => "Oznaka:",
+		'addList' => "Ustvari nov seznam",
+		'renameList' => "Preimenjuj seznam",
+		'deleteList' => "Ta ukaz bo izbrisal tudi vse naloge, v tem seznamu.\\nAli ste prepričani?",
+		'settingsSaved' => "Nastavitve shranjene. Posodabljam...",
+	);
+
+	var $inc = array
+	(
+		'My Tiny Todolist' => "Moj Seznam Nalog",
+		'htab_newtask' => "Naloga",
+		'htab_search' => "Iskalnik",
+		'btn_add' => "Dodaj",
+		'btn_search' => "Išči",
+		'advanced_add' => "Napredno",
+		'searching' => "Searching for",
+		'tasks' => "Naloge",
+		'taskdate_inline' => "dodano %s",
+		'taskdate_created' => "Datum nastanka",
+		'taskdate_completed' => "Datum zaključka",
+		'edit_task' => "Uredi nalogo",
+		'add_task' => "Nova Naloga",
+		'priority' => "Pomembnost",
+		'task' => "Naloga",
+		'note' => "Beležka",
+		'save' => "Shrani",
+		'cancel' => "Prekliči",
+		'password' => "Geslo",
+		'btn_login' => "Prijava",
+		'a_login' => "Prijava",
+		'a_logout' => "Odjava",
+		'public_tasks' => "Javne Naloge",
+		'tags' => "Oznake",
+		'tagfilter_cancel' => "ročno sortiraj",
+		'sortByHand' => "Sortiraj ročno",
+		'sortByPriority' => "Sortiraj po pomembnosti",
+		'sortByDueDate' => "Sortiraj po zapadlosti",
+		'due' => "zapadlost",
+		'daysago' => "pred %d",
+		'indays' => "čez %d dni",
+		'months_short' => array("Jan","Feb","Mar","Apr","Maj","Jun","Jul","Avg","Sep","Okt","Nov","Dec"),
+		'months_long' => array("Januar","Februar","Marc","April","Maj","Junij","Julij","Avgust","September","Oktober","November","December"),
+		'days_min' => array("Ned","Pon","Tor","Sre","Čet","Pet","Sob"),
+		'days_long' => array("Nedelja","Ponedeljek","Torek","Sreda","Četrtek","Petek","Sobota"),
+		'today' => "danes",
+		'yesterday' => "včeraj",
+		'tomorrow' => "jutri",
+		'f_past' => "čez rok",
+		'f_today' => "Danes in jutri",
+		'f_soon' => "Kmalu",
+		'tasks_and_compl' => "Naloge + končane",
+		'action_edit' => "Uredi",
+		'action_note' => "Uredi Beležko",
+		'action_delete' => "Odstrani",
+		'action_priority' => "Pomembnost",
+		'action_move' => "Premakni v",
+		'notes' => "Beležke:",
+		'notes_show' => "Skrij",
+		'notes_hide' => "Prikaži",
+		'list_new' => "Nov seznam",
+		'list_rename' => "Preimenjuj seznam",
+		'list_delete' => "Odstrani seznam",
+		'list_publish' => "Objavi seznam",
+		'alltags' => "Vse oznake:",
+		'alltags_show' => "Prikaži vse",
+		'alltags_hide' => "Skrij vse",
+		'a_settings' => "Nastavitve",
+		'rss_feed' => "RSS Vir",
+		'feed_title' => "%s",
+		'feed_description' => "Nova naloga v %s",
+
+		/* Settings */
+		'set_header' => "Nastavitve",
+		'set_title' => "Naslov",
+		'set_title_descr' => "(uredi če želiš spremeniti privzeti naslov)",
+		'set_language' => "Jezik vmesnika",
+		'set_protection' => "Zaščiteno z geslom",
+		'set_enabled' => "Vključeno",
+		'set_disabled' => "Izključeno",
+		'set_newpass' => "Geslo",
+		'set_newpass_descr' => "(pusti polje prazno, če ne želiš spremeniti gesla)",
+		'set_smartsyntax' => "Pametne vnos",
+		'set_smartsyntax_descr' => "(/pomembnost/ naloga /oznake/)",
+		'set_autotz' => "Samodejni časovni pas",
+		'set_autotz_descr' => "(zazna s pomočjo spletnega brskalnika in javaskripte",
+		'set_autotag' => "Samodejne oznake",
+		'set_autotag_descr' => "(samodejno doda oznako trenutno izbranega filtra oznake)",
+		'set_sessions' => "Shranjevanje seje",
+		'set_sessions_php' => "PHP",
+		'set_sessions_files' => "Datoteka",
+		'set_firstdayofweek' => "Prvi dan v tednu",
+		'set_duedate' => "Format koledarja za zapadlost",
+		'set_date' => "Format datuma",
+		'set_shortdate' => "Kratki format datuma",
+		'set_clock' => "Format prikaza ure",
+		'set_12hour' => "12-urni",
+		'set_24hour' => "24-urni",
+		'set_submit' => "Shrani spremembe",
+		'set_cancel' => "Prekliči",
+	);
+}
+
+?>
