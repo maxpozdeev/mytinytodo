f3d52cbe4c21814008afc68e66cdeffdd1349385
Max Pozdeev <maxpozdeev@gmail.com>
2010-01-21 20:43:35 +0300

    langs: Spanish, French - updated; Persian,Hungarian - added

---
+++ langs/fa.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+	myTinyTodo language pack
+	Language: Persian
+	Original name: فارسی
+	Author: Mostafa Khademolmele
+	Author Url: http://www.nemov.ir
+	AppVersion: v1.3.2
+	Date: 2010-01-02
+*/
+
+class Lang extends DefaultLang
+{
+	var $rtl = 1;
+	var $js = array
+	(
+		'actionNote' => "یادداشت",
+		'actionEdit' => "ویرایش",
+		'actionDelete' => "حذف",
+		'confirmDelete' => "آیا مطمئن هستید؟",
+		'actionNoteSave' => "ثبت",
+		'actionNoteCancel' => "انصراف",
+		'error' => "Some error occurred (click for details)",
+		'denied' => "عدم دسترسی",
+		'invalidpass' => "گذرواژه نادرست",
+		'readonly' => "read-only",
+		'tagfilter' => "برچسب:",
+		'addList' => "ایجاد فهرست جدید",
+		'renameList' => "تغییر نام فهرست",
+		'deleteList' => "This will delete current list with all tasks in it.\\nAre you sure?",
+		'settingsSaved' => "تنظیمات ثبت شد. بارگذاری مجدد...",
+	);
+
+	var $inc = array
+	(
+		'My Tiny Todolist' => "مدیریت وظایف",
+		'htab_newtask' => "وظیفه جدید",
+		'htab_search' => "جستجو",
+		'btn_add' => "جدید",
+		'btn_search' => "جستجو",
+		'advanced_add' => "پیشرفته",
+		'searching' => "جستجو برای ",
+		'tasks' => "وظایف",
+		'taskdate_inline' => "اضافه شده در %s",
+		'taskdate_created' => "تاریخ ایجاد",
+		'taskdate_completed' => "تاریخ تکمیل",
+		'edit_task' => "ویرایش وظیفه",
+		'add_task' => "وظیفه جدید",
+		'priority' => "الویت",
+		'task' => "وظیفه",
+		'note' => "یادداشت",
+		'save' => "ثبت",
+		'cancel' => "انصراف",
+		'password' => "گذرواژه",
+		'btn_login' => "ورود",
+		'a_login' => "ورود",
+		'a_logout' => "خروج",
+		'public_tasks' => "وظایف عمومی",
+		'tags' => "برچسب",
+		'tagfilter_cancel' => "انصراف از فیلتر",
+		'sortByHand' => "ترتیب دستی",
+		'sortByPriority' => "ترتیب الویت",
+		'sortByDueDate' => "ترتیب تاریخ تحویل",
+		'due' => "Due",
+		'daysago' => "%d روز قبل",
+		'indays' => "در %d روز",
+		'months_short' => array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"),
+		'months_long' => array("January","February","March","April","May","June","July","August","September","October","November","December"),
+		'days_min' => array("ی","د","س","چ","پ","ج","ش"),
+		'days_long' => array("یک شنبه","دو شنبه","سه شنبه","چهار شنبه","پنج شنبه","جمعه","شنبه"),
+		'today' => "امروز",
+		'yesterday' => "دیروز",
+		'tomorrow' => "فردا",
+		'f_past' => "Overdue",
+		'f_today' => "امروز و فردا",
+		'f_today' => "امروز و فردا",
+		'f_soon' => "زود",
+		'tasks_and_compl' => "وظایف + تکمیل شده ها",
+		'action_edit' => "ویرایش",
+		'action_note' => "ویرایش یادداشت",
+		'action_delete' => "حذف",
+		'action_priority' => "اولویت",
+		'action_move' => "انتقال به",
+		'notes' => "یادداشت ها:",
+		'notes_show' => "نمایش",
+		'notes_hide' => "عدم نمایش",
+		'list_new' => "فهرست جدید",
+		'list_rename' => "تغییر نام",
+		'list_delete' => "حذف",
+		'list_publish' => "انتشار",
+		'alltags' => "همه برچسب ها:",
+		'alltags_show' => "نمایش همه",
+		'alltags_hide' => "عدم نمایش همه",
+		'a_settings' => "تنظیمات",
+		'rss_feed' => "RSS Feed",
+		'feed_title' => "%s",
+		'feed_description' => "وظیفه جدید در %s",
+		/* Settings */
+		'set_header' => "تنظیمات",
+		'set_title' => "عنوان",
+		'set_title_descr' => "(specify if you want to change default title)",
+		'set_language' => "زبان",
+		'set_protection' => "Password protection",
+		'set_enabled' => "فعال",
+		'set_disabled' => "غیرفعال",
+		'set_newpass' => "گذرواژه جدید",
+		'set_newpass_descr' => "(leave blank if won't change current password)",
+		'set_smartsyntax' => "Smart syntax",
+		'set_smartsyntax_descr' => "(/priority/ task /tags/)",
+		'set_autotz' => "Automatic timezone",
+		'set_autotz_descr' => "(determines timezone offset of user environment with javascript)",
+		'set_autotag' => "Autotagging",
+		'set_autotag_descr' => "(automatically adds tag of current tag filter to newly created task)",
+		'set_sessions' => "Session handling mechanism",
+		'set_sessions_php' => "PHP",
+		'set_sessions_files' => "Files",
+		'set_firstdayofweek' => "اولین روز هفته",
+		'set_duedate' => "Duedate calendar format",
+		'set_date' => "قالب تاریخ",
+		'set_shortdate' => "قالب کوتاه تاریخ",
+		'set_clock' => "قالب ساعت",
+		'set_12hour' => "12-ساعتی",
+		'set_24hour' => "24-ساعتی",
+		'set_submit' => "ثبت تغییرات",
+		'set_cancel' => "انصراف",
+	);
+}
+
+?>
---
+++ langs/hu.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+	myTinyTodo language pack
+	Language: Hungarian
+	Original name: Magyar
+	Author: Jozsef Kollar
+	Author Url: http://www.bassline.hu
+	AppVersion: v1.3.2
+	Date: 2010-01-20
+*/
+
+class Lang extends DefaultLang
+{
+	var $js = array
+	(
+		'confirmDelete' => "Biztosan törölni akarod a feladatot?",
+		'actionNoteSave' => "Mentés",
+		'actionNoteCancel' => "Mégse",
+		'error' => "Hiba lépett fel (kattints a részletekért)",
+		'denied' => "Hozzáférés megtagadva",
+		'invalidpass' => "Hibás jelszó",
+		'tagfilter' => "Cimke:",
+		'addList' => "Új Lista",
+		'renameList' => "Lista átnevezése",
+		'deleteList' => "Az aktuális Lista törlése az összes Feladattal.\\nBiztos benne?",
+		'settingsSaved' => "Beállítások mentve. Újratöltés...",
+	);
+
+	var $inc = array
+	(
+		'My Tiny Todolist' => "My Tiny Todolist",
+		'htab_newtask' => "Új Feladat",
+		'htab_search' => "Keresés",
+		'btn_add' => "Hozzáad",
+		'btn_search' => "Keresés",
+		'advanced_add' => "Részletes",
+		'searching' => "A következő keresése",
+		'tasks' => "Feladatok",
+		'taskdate_inline' => "hozzáadva %s",
+		'taskdate_created' => "Létrehozás dátuma",
+		'taskdate_completed' => "Befejezés dátuma",
+		'edit_task' => "Feladat szerkesztése",
+		'add_task' => "Új Feladat",
+		'priority' => "Prioritás",
+		'task' => "Feladat",
+		'note' => "Megjegyzés",
+		'save' => "Mentés",
+		'cancel' => "Mégse",
+		'password' => "Jelszó",
+		'btn_login' => "Belépés",
+		'a_login' => "Belépés",
+		'a_logout' => "Kilépés",
+		'public_tasks' => "Közös Feladatok",
+		'tags' => "Cimkék",
+		'tagfilter_cancel' => "szűrő kikapcsolása",
+		'sortByHand' => "Rendezés kézzel",
+		'sortByPriority' => "Rendezés prioritás szerint",
+		'sortByDueDate' => "Rendezés dátum szerint",
+		'due' => "Esedékes",
+		'daysago' => "%d nappal ezelött",
+		'indays' => "%d nap múlva",
+		'months_short' => array("Jan","Feb","Már","Ápr","Máj","Jún","Júl","Aug","Szep","Okt","Nov","Dec"),
+		'months_long' => array("Január","Február","Március","Április","Május","Június","Július","Augusztus","Szeptember","Október","November","December"),
+		'days_min' => array("V","H","K","Sz","Cs","P","Sz"),
+		'days_long' => array("Vasárnap","Hétfő","Kedd","Szerda","Csütörtök","Péntek","Szombat"),
+		'today' => "Ma",
+		'yesterday' => "Tegnap",
+		'tomorrow' => "Holnap",
+		'f_past' => "Lejárt",
+		'f_today' => "Ma és holnap",
+		'f_soon' => "Hamarosan",
+		'tasks_and_compl' => "Feladatok + Lejártak",
+		'action_edit' => "Szerkesztés",
+		'action_note' => "Megjegyzés szerkesztése",
+		'action_delete' => "Törlés",
+		'action_priority' => "Prioritás",
+		'action_move' => "Mozgatás ide",
+		'notes' => "Megjegyzés:",
+		'notes_show' => "Mutat",
+		'notes_hide' => "Elrejt",
+		'list_new' => "Új lista",
+		'list_rename' => "Lista átnevezése",
+		'list_delete' => "Lista törlése",
+		'list_publish' => "Lista közzététele",
+		'alltags' => "Összes Cimke:",
+		'alltags_show' => "Összes mutatása",
+		'alltags_hide' => "Összes elrejtése",
+		'a_settings' => "Beállítások",
+		'rss_feed' => "RSS",
+		'feed_title' => "%s",
+		'feed_description' => "Új Feladat itt -> %s",
+
+		/* Settings */
+		'set_header' => "Beállítások",
+		'set_title' => "Cím",
+		'set_title_descr' => "(add meg ha megszeretnéd változtatni az alapértelmezett címet)",
+		'set_language' => "Nyelv",
+		'set_protection' => "Védelem jelszóval",
+		'set_enabled' => "Bekapcsolva",
+		'set_disabled' => "Kikapcsolva",
+		'set_newpass' => "Új jelszó",
+		'set_newpass_descr' => "(hagyd üresen ha nem szeretnéd megváltoztatni a jelenlegi jelszót)",
+		'set_smartsyntax' => "Gyors sorrend",
+		'set_smartsyntax_descr' => "(/prioritás/ feladatok / cimkék/)",
+		'set_autotz' => "Automatikus időzóna",
+		'set_autotz_descr' => "(autómatikus időzona meghatározás java script-tel)",
+		'set_autotag' => "Autómatikus cimkézés",
+		'set_autotag_descr' => "(automatikusan az aktuális cimke hozzárendelése)",
+		'set_sessions' => "a feladat kezelés módja",
+		'set_sessions_php' => "PHP",
+		'set_sessions_files' => "Files",
+		'set_firstdayofweek' => "A hét első napja",
+		'set_duedate' => "Határidő naptár formája",
+		'set_date' => "Dátum formátum",
+		'set_shortdate' => "Rövid dátum formátum",
+		'set_clock' => "Óra formátum",
+		'set_12hour' => "12-óra",
+		'set_24hour' => "24-óra",
+		'set_submit' => "Változások mentése",
+		'set_cancel' => "Mégse",
+	);
+}
+
+?>
