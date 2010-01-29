7432e1941449fc2b8b9289b4a0d3066feb74ad1e
Max Pozdeev <maxpozdeev@gmail.com>
2010-01-29 17:32:35 +0300

    Italian translation updated (thanks to Giuseppe Dessм)

--- langs/it.php
+++ langs/it.php
@@ -1,67 +1,125 @@
 <?php
 
-/**
- * Italian translation for My Tiny Todolist v1.2.5
- * Author: Devis Lucato http://www.lucato.it
- */
+/*
+	myTinyTodo language pack
+	Language: Italian
+	Original name: Italiano
+	Author: Giuseppe Dessì
+	Author Url:
+	AppVersion: v1.3.2
+	Date: 2010-01-24
+*/
 
 class Lang extends DefaultLang
 {
-	var $js = array
-	(
-		'actionNote' => "nota",
-		'actionEdit' => "modifica",
-		'actionDelete' => "elimina",
-		'taskDate' => array("function(date) { return 'aggiunto il '+date; }"),
-		'confirmDelete' => "Sicuro?",
-		'actionNoteSave' => "salva",
-		'actionNoteCancel' => "annulla",
-		'error' => "Si è verificato un errore (clicca per maggiori dettagli)",
-		'denied' => "Accesso negato",
-		'invalidpass' => "Password errata",
-		'readonly' => "sola lettura",
-		'tagfilter' => "Tag:",
-	);
 
-	var $inc = array
-	(
-		'My Tiny Todolist' => "I miei compiti",
-		'tab_newtask' => "nuovo compito",
-		'tab_search' => "cerca",
-		'btn_add' => "Aggiungi",
-		'btn_search' => "Cerca",
-		'searching' => "Cercando",
-		'tasks' => "Compiti",
-		'edit_task' => "Modifica Compito",
-		'priority' => "Priorità",
-		'task' => "Compito",
-		'note' => "Nota",
-		'save' => "Salva",
-		'cancel' => "Annula",
-		'password' => "Password",
-		'btn_login' => "Login",
-		'a_login' => "Login",
-		'a_logout' => "Esci",
-		'tags' => "Tags",
-		'tagfilter_cancel' => "annulla filtro",
-		'sortByHand' => "Ordine manuale",
-		'sortByPriority' => "Ordina per priorità",
-		'sortByDueDate' => "Ordina per scadenza",
-		'due' => "Scadenza",
-		'daysago' => "%d giorni fa",
-		'indays' => "in %d giorni",
-		'months_short' => array("Gen","Feb","Mar","Apr","Mag","Giu","Lug","Ago","Set","Ott","Nov","Dic"),
-		'days_min' => array("Do","Lu","Ma","Me","Gi","Ve","Sa"),
-		'date_md' => "%1\$s %2\$d",
-		'date_ymd' => "%2\$s %3\$d, %1\$d",
-		'today' => "oggi",
-		'yesterday' => "ieri",
-		'tomorrow' => "domani",
-		'f_past' => "In ritardo",
-		'f_today' => "Oggi e domani",
-		'f_soon' => "Presto",
-		'tasks_and_compl' => "Compiti + completati",
-	);
+	    var $js = array
+    (
+        'confirmDelete' => "Sei sicuro di voler cancellare il task?",
+        'actionNoteSave' => "salva",
+        'actionNoteCancel' => "annulla",
+        'error' => "Ci sono errori (clicca per dettagli)",
+        'denied' => "Accesso non consentito",
+        'invalidpass' => "Password errata",
+        'tagfilter' => "Tag:",
+        'addList' => "Crea una nuova lista",
+        'renameList' => "Rinomina lista list",
+        'deleteList' => "Questo eliminerà la lista corrente e tutti i task inclusi.\\nSei sicuro?",
+        'settingsSaved' => "Impostazioni salvate. Ricaricando...",
+    );
+	    var $inc = array
+    (
+        'My Tiny Todolist' => "Tasks",
+        'htab_newtask' => "Nuovo Task",
+        'htab_search' => "Cerca",
+        'btn_add' => "Aggiungi",
+        'btn_search' => "Cerca",
+        'advanced_add' => "Avanzato",
+        'searching' => "Cercando",
+        'tasks' => "Tasks",
+        'taskdate_inline' => "aggiunto il %s",
+        'taskdate_created' => "Data di creazione",
+        'taskdate_completed' => "Data di scadenza",
+        'edit_task' => "Modifica Task",
+        'add_task' => "Nuovo Task",
+        'priority' => "Priorità",
+        'task' => "Task",
+        'note' => "Nota",
+        'save' => "Salva",
+        'cancel' => "Annulla",
+        'password' => "Password",
+        'btn_login' => "Login",
+        'a_login' => "Login",
+        'a_logout' => "Logout",
+        'public_tasks' => "Tasks Pubblici",
+        'tags' => "Tags",
+        'tagfilter_cancel' => "annulla filtro",
+        'sortByHand' => "Ordina",
+        'sortByPriority' => "Ordina per priorità",
+        'sortByDueDate' => "Ordina per scadenza",
+        'due' => "Scadenza",
+        'daysago' => "%d giorni fa",
+        'indays' => "entro %d giorni",
+        'months_short' => array("Gen","Feb","Mar","Apr","Mag","Giu","Lug","Ago","Set","Ott","Nov","Dic"),
+        'months_long' => array("Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"),
+        'days_min' => array("Do","Lu","Ma","Me","Gi","Ve","Sa"),
+        'days_long' => array("Domenica","Lunedì","martedì","Mercoledì","Giovedì","Venerdì","Sabato"),
+        'today' => "oggi",
+        'yesterday' => "ieri",
+        'tomorrow' => "domani",
+        'f_past' => "Scaduto",
+        'f_today' => "Oggi e domani",
+        'f_soon' => "Prossimamente",
+        'tasks_and_compl' => "Tasks + completi",
+        'action_edit' => "Modifica",
+        'action_note' => "Modifica Nota",
+        'action_delete' => "Elimina",
+        'action_priority' => "Priorità",
+        'action_move' => "sposta in",
+        'notes' => "Note:",
+        'notes_show' => "Mostra",
+        'notes_hide' => "Nascondi",
+        'list_new' => "Nuova lista",
+        'list_rename' => "Rinomina lista",
+        'list_delete' => "Elimina lista",
+        'list_publish' => "Pubblica lista",
+        'alltags' => "Tutti i tags:",
+        'alltags_show' => "Visualizza tutti",
+        'alltags_hide' => "Nacondi tutti",
+        'a_settings' => "Impostazioni",
+        'rss_feed' => "RSS Feed",
+        'feed_title' => "%s",
+        'feed_description' => "Nuovi tasks in %s",
+
+        /* Settings */
+        'set_header' => "Impostazioni",
+        'set_title' => "Titolo",
+        'set_title_descr' => "(specifica se vuoi cambiare il titolo predefinito)",
+        'set_language' => "Linguaggio",
+        'set_protection' => "Protezione con password",
+        'set_enabled' => "Attivo",
+        'set_disabled' => "Disattivo",
+        'set_newpass' => "Nuova password",
+        'set_newpass_descr' => "(non compilare se non vuoi cambiare password)",
+        'set_smartsyntax' => "Smart syntax",
+        'set_smartsyntax_descr' => "(/priority/ task /tags/)",
+        'set_autotz' => "Fuso orario automatico",
+        'set_autotz_descr' => "(determina il fuso orario per essere utilizzato con gli javascript)",
+        'set_autotag' => "Tagging automatico",
+        'set_autotag_descr' => "(aggiunge automaticamente i tag per i nuovi compiti)",
+        'set_sessions' => "Meccanismo di gestione sessione",
+        'set_sessions_php' => "PHP",
+        'set_sessions_files' => "Files",
+        'set_firstdayofweek' => "Primo giorno della settimana",
+        'set_duedate' => "Formato data di scadenza",
+        'set_date' => "Formato data",
+        'set_shortdate' => "Formato data abbreviata",
+        'set_clock' => "Formato orario",
+        'set_12hour' => "12-hour",
+        'set_24hour' => "24-hour",
+        'set_submit' => "applica le modifiche",
+        'set_cancel' => "Annulla",
+    );
 }
 
 ?>
