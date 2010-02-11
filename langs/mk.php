3fce9df852eabdb01830cd50ac66443a05236730
Max Pozdeev <maxpozdeev@gmail.com>
2010-02-11 09:37:45 +0300

    Macedonian language updated (thanks to Kosta, nGen Solutions)

--- langs/mk.php
+++ langs/mk.php
@@ -1,39 +1,47 @@
 <?php
 
 /*
-	myTinyTodo language pack: Macedonian
-	by nGen Solutions (http://www.ngen.mk)
-	v1.2 compatible 
+	myTinyTodo language pack
+	Language: Macedonian
+	Original name: Македонски
+	Author: nGen Solutions
+	Author Url: http://ngen.mk
+	AppVersion: v1.3.3
+	Date: 2010-02-11
 */
 
 class Lang extends DefaultLang
 {
 	var $js = array
 	(
-		'actionNote' => "забелешка",
-		'actionEdit' => "измени",
-		'actionDelete' => "избриши",
-		'taskDate' => array("function(date) { return 'added at '+date; }"),
 		'confirmDelete' => "Дали си сигурен?",
 		'actionNoteSave' => "зачувај",
 		'actionNoteCancel' => "откажи",
 		'error' => "Се појави грешка... (подетално)",
 		'denied' => "Забранет пристап",
 		'invalidpass' => "Неточна лозинка",
-		'readonly' => "read-only",
 		'tagfilter' => "Ознака:",
-	);
+		'addList' => "Направи нова листа",
+		'renameList' => "Преименувај листа",
+		'deleteList' => "Со ова ке ја избишете листата и сите задачи во неа.\\nПродолжи?",
+		'settingsSaved' => "Промените зачувани. Вчитувам...",
+		);
 
 	var $inc = array
 	(
-		'My Tiny Todolist' => "Моја Листа на Задачи",
-		'tab_newtask' => "нова задача",
-		'tab_search' => "пребарај",
+		'My Tiny Todolist' => "Листа на Задачи",
+		'htab_newtask' => "Нова задача",
+		'htab_search' => "Пребарај",
 		'btn_add' => "Додади",
 		'btn_search' => "Барај",
-		'searching' => "Пребарувам",
+		'advanced_add' => "Напредно",
+		'searching' => "Пребарувам за",
 		'tasks' => "Задачи",
+		'taskdate_inline' => "додадена во %s",
+		'taskdate_created' => "Креирана",
+		'taskdate_completed' => "Завршена",
 		'edit_task' => "Измени задача",
+		'add_task' => "Нова Задача",
 		'priority' => "Приоритет",
 		'task' => "Задача",
 		'note' => "Забелешка",
@@ -43,6 +51,7 @@ class Lang extends DefaultLang
 		'btn_login' => "Најави се",
 		'a_login' => "Најава",
 		'a_logout' => "Одјави се",
+		'public_tasks' => "Јавни задачи",
 		'tags' => "Ознаки",
 		'tagfilter_cancel' => "откажи филтер",
 		'sortByHand' => "Подреди рачно",
@@ -54,15 +63,62 @@ class Lang extends DefaultLang
 		'months_short' => array("Јан","Фев","Мар","Апр","Мај","Јун","Јул","Авг","Сеп","Окт","Нов","Дек"),
 		'months_long' => array("Јануари","Февруари","Март","Април","Мај","Јуни","Јули","Август","Септември","Октомври","Ноември","Декември"),
 		'days_min' => array("Не","По","Вт","Ср","Че","Пе","Са"),
-		'date_md' => "%1\$s %2\$d",
-		'date_ymd' => "%2\$s %3\$d, %1\$d",
+		'days_long' => array("Недела","Понеделник","Вторник","Среда","Четврток","Петок","Сабота"),
 		'today' => "денес",
 		'yesterday' => "вчера",
-		'tomorrow' => "Утре",
+		'tomorrow' => "утре",
 		'f_past' => "Задоцнети",
-		'f_today' => "Денес и Утре",
-		'f_soon' => "Наскоро",
-		'tasks_and_compl' => "Задачи + завршени",
+		'f_today' => "денес и утре",
+		'f_soon' => "наскоро",
+		'action_edit' => "Измени",
+		'action_note' => "Измени забелешка",
+		'action_delete' => "Избриши",
+		'action_priority' => "Приоритет",
+		'action_move' => "Премести во",
+		'notes' => "Забелешки:",
+		'notes_show' => "Прикажи",
+		'notes_hide' => "Сокриј",
+		'list_new' => "Нова листа",
+		'list_rename' => "Преименувај листа",
+		'list_delete' => "Избриши листа",
+		'list_publish' => "Објави листа",
+		'list_showcompleted' => "Покажи завршени задачи",
+		'alltags' => "Сите ознаки:",
+		'alltags_show' => "Прикажи ги сите",
+		'alltags_hide' => "Сокриј ги сите",
+		'a_settings' => "Подесувања",
+		'rss_feed' => "RSS достава",
+		'feed_title' => "%s",
+		'feed_description' => "Нови задачи за %s",
+
+		/* Settings */
+		'set_header' => "Подесувања",
+		'set_title' => "Наслов",
+		'set_title_descr' => "(промени го името на програмата)",
+		'set_language' => "Јазик",
+		'set_protection' => "Заштита со лозинка",
+		'set_enabled' => "Вклучи",
+		'set_disabled' => "Исклучи",
+		'set_newpass' => "Нова лозинка",
+		'set_newpass_descr' => "(празно нема да ја смени лозинката)",
+		'set_smartsyntax' => "Паметен приказ",
+		'set_smartsyntax_descr' => "(/приоритет/ задача /ознаки/)",
+		'set_autotz' => "Автоматска временска зона",
+		'set_autotz_descr' => "(ја одредува временската зона со помош на скрипта)",
+		'set_autotag' => "Автоматски ознаки",
+		'set_autotag_descr' => "(автоматски ја додава филтрираната ознака на новокреираната задача)",
+		'set_sessions' => "Механизам за справување со сесијата",
+		'set_sessions_php' => "PHP",
+		'set_sessions_files' => "Со Фајлови",
+		'set_firstdayofweek' => "Прв ден од неделата",
+		'set_duedate' => "Приказ на календар",
+		'set_date' => "Приказ на дата",
+		'set_shortdate' => "Краток формат",
+		'set_clock' => "Приказ на време",
+		'set_12hour' => "12-часа",
+		'set_24hour' => "24-часа",
+		'set_submit' => "Зачувај ги промените",
+		'set_cancel' => "Откажи",
 	);
 }
 
