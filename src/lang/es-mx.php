c06e2f866a5f256b05182cd23c703194006dd4ec
Max Pozdeev <maxpozdeev@gmail.com>
2010-08-12 11:42:37 +0400

    Spanish (Mexico) translation added (by eNK-Psy)

---
+++ src/lang/es-mx.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+	myTinyTodo language pack
+	Language: Spanish (Mexico)
+	Original name: Español (de México)
+	Author: eNK-Psy
+	Author Url: http://www.buscachilpo.com.mx
+	AppVersion: v1.3.6
+	Date: 2010-08-03
+*/
+
+class Lang extends DefaultLang
+{
+	var $js = array
+	(
+		'confirmDelete' => "¿Está seguro que desea eliminar la tarea?",
+		'actionNoteSave' => "guardar",
+		'actionNoteCancel' => "cancelar",
+		'error' => "Algun error a ocurrido (haga clic para más detalles)",
+		'denied' => "Acceso denegado",
+		'invalidpass' => "Contraseña incorrecta",
+		'tagfilter' => "Etiqueta:",
+		'addList' => "Crear una nueva lista",
+		'renameList' => "Renombrar lista",
+		'deleteList' => "Esto eliminará la lista actual con todas las tareas en ella.\\n¿Está seguro?",
+		'clearCompleted' => "Esto eliminará todas las tareas completadas en la lista.\\n¿Está seguro?",
+		'settingsSaved' => "Configuración guardada. Abriendo...",
+	);
+
+	var $inc = array
+	(
+		'My Tiny Todolist' => "My Tiny Todolist",
+		'htab_newtask' => "Nueva tarea",
+		'htab_search' => "Búsqueda",
+		'btn_add' => "Añadir",
+		'btn_search' => "Búsqueda",
+		'advanced_add' => "Avanzado",
+		'searching' => "En busca de",
+		'tasks' => "Tareas",
+		'taskdate_inline' => "añadido a %s",
+		'taskdate_created' => "Fecha de creación",
+		'taskdate_completed' => "Fecha de la terminación",
+		'edit_task' => "Editar Tarea",
+		'add_task' => "Nueva tarea",
+		'priority' => "Prioridad",
+		'task' => "Tarea",
+		'note' => "Nota",
+		'save' => "Guardar",
+		'cancel' => "Cancelar",
+		'password' => "Contraseña",
+		'btn_login' => "Autentificarse",
+		'a_login' => "Autentificarse",
+		'a_logout' => "Cerrar sesión",
+		'public_tasks' => "funciones públicas",
+		'tags' => "Etiquetas",
+		'tagfilter_cancel' => "cancelar filtro",
+		'sortByHand' => "Ordenar a mano",
+		'sortByPriority' => "Ordenar por prioridad",
+		'sortByDueDate' => "Ordenar por fecha de vencimiento",
+		'due' => "Vencimiento",
+		'daysago' => "Hace %d días",
+		'indays' => "en %d días",
+		'months_short' => array("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dec"),
+		'months_long' => array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"),
+		'days_min' => array("Dom","Lun","Mar","Mie","Jue","Vie","Sab"),
+		'days_long' => array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"),
+		'today' => "hoy",
+		'yesterday' => "ayer",
+		'tomorrow' => "mañana",
+		'f_past' => "Atrasado",
+		'f_today' => "Hoy y mañana",
+		'f_soon' => "Pronto",
+		'action_edit' => "Editar",
+		'action_note' => "Editar Nota",
+		'action_delete' => "Borrar",
+		'action_priority' => "Prioridad",
+		'action_move' => "Mover a",
+		'notes' => "Notas:",
+		'notes_show' => "Mostrar",
+		'notes_hide' => "Ocultar",
+		'list_new' => "Nueva lista",
+		'list_rename' => "Renombrar lista",
+		'list_delete' => "Borrar lista",
+		'list_publish' => "Publicar lista",
+		'list_showcompleted' => "Mostrar tareas completadas",
+		'list_clearcompleted' => "Borrar las tareas completadas",
+		'alltags' => "Todas las etiquetas:",
+		'alltags_show' => "Mostrar todo",
+		'alltags_hide' => "Ocultar todo",
+		'a_settings' => "Configuración",
+		'rss_feed' => "RSS Feed",
+		'feed_title' => "%s",
+		'feed_description' => "Nuevas tareas en %s",
+
+		/* Settings */
+		'set_header' => "Configuración",
+		'set_title' => "Título",
+		'set_title_descr' => "(especifica si desea cambiar el título por defecto)",
+		'set_language' => "Idioma",
+		'set_protection' => "Contraseña de protección",
+		'set_enabled' => "Habilitado",
+		'set_disabled' => "Desabilitado",
+		'set_newpass' => "Nueva contraseña",
+		'set_newpass_descr' => "(deja en blanco si no quiere cambiar la contraseña actual)",
+		'set_smartsyntax' => "Sintaxis inteligente",
+		'set_smartsyntax_descr' => "(/prioridad/ tarea /etiqueta/)",
+		'set_autotz' => "Zona horaria automática",
+		'set_autotz_descr' => "(determina desplazamiento de zona horaria de entorno de usuario con javascript)",
+		'set_autotag' => "Autoetiquetado",
+		'set_autotag_descr' => "(agrega automáticamente la etiqueta de filtro de etiqueta actual a la tarea de nueva creación)",
+		'set_sessions' => "Mecanismo de manejo de sesión",
+		'set_sessions_php' => "PHP",
+		'set_sessions_files' => "Archivos",
+		'set_firstdayofweek' => "Primer día de la semana",
+		'set_duedate' => "Formato de fecha de venciomiento de calendario",
+		'set_date' => "Formato de fecha",
+		'set_shortdate' => "Ordenar formato de fecha",
+		'set_clock' => "Formato de reloj",
+		'set_12hour' => "12-horas",
+		'set_24hour' => "24-horas",
+		'set_submit' => "Enviar cambios",
+		'set_cancel' => "Cancelar",
+		'set_showdate' => "Mostrar fecha de tarea en lista",
+	);
+}
+
+?>
