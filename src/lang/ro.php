d1b0f51537b5ce4c35ced6d252227b9393faa330
Max Pozdeev <maxpozdeev@gmail.com>
2010-09-08 09:16:22 +0400

    Romanian translation added (by Alin-Andrei Chican)

---
+++ src/lang/ro.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+	myTinyTodo language pack
+	Language: Romanian
+	Original name: română
+	Author: Alin-Andrei Chican
+	Author Url: http://www.chican.ro
+	AppVersion: v1.3.6
+	Date: 2010-09-08
+*/
+
+class Lang extends DefaultLang
+{
+	var $js = array
+	(
+		'confirmDelete' => "Ești sigur că vrei să ștergi sarcina?",
+		'actionNoteSave' => "salvează",
+		'actionNoteCancel' => "anulează",
+		'error' => "A avut loc o eroare (click pentru detalii)",
+		'denied' => "Acces interzis",
+		'invalidpass' => "Parolă greșită",
+		'tagfilter' => "Etichetă:",
+		'addList' => "Crează o nouă listă",
+		'renameList' => "Redenumește listă",
+		'deleteList' => "Această acțiune va șterge lista curentă cu toate sarcinile conținute.\\nEști sigur?",
+		'clearCompleted' => "Această acțiune va șterge toate sarcinile efectuate din listă.\\nEști sigur?",
+		'settingsSaved' => "Setări salvate. Împrospătare...",
+	);
+
+	var $inc = array
+	(
+		'My Tiny Todolist' => "Lista mea mică de sarcini",
+		'htab_newtask' => "Sarcină nouă",
+		'htab_search' => "Căutare",
+		'btn_add' => "Adaugă",
+		'btn_search' => "Caută",
+		'advanced_add' => "Avansat",
+		'searching' => "Căutare după",
+		'tasks' => "Sarcini",
+		'taskdate_inline' => "adăugat la %s",
+		'taskdate_created' => "Data creării",
+		'taskdate_completed' => "Data efectuării",
+		'edit_task' => "Modifică sarcină",
+		'add_task' => "Sarcină nouă",
+		'priority' => "Prioritate",
+		'task' => "Sarcină",
+		'note' => "Notiță",
+		'save' => "Salvează",
+		'cancel' => "Anulează",
+		'password' => "Parolă",
+		'btn_login' => "Autentifică-te",
+		'a_login' => "Autentificare",
+		'a_logout' => "Deautentificare",
+		'public_tasks' => "Sarcini publice",
+		'tags' => "Etichete",
+		'tagfilter_cancel' => "anulează filtru",
+		'sortByHand' => "Sortează manual",
+		'sortByPriority' => "Sortează după prioritate",
+		'sortByDueDate' => "Sortează după data limită",
+		'due' => "Data limită",
+		'daysago' => "acum %d zile",
+		'indays' => "în %d zile",
+		'months_short' => array("ian","feb","mar","apr","mai","iun","iul","aug","sep","oct","nov","dec"),
+		'months_long' => array("ianuarie","februarie","martie","aprilie","mai","iunie","iulie","august","septembrie","octombrie","noiembrie","decembrie"),
+		'days_min' => array("du","lu","ma","mi","jo","vi","sa"),
+		'days_long' => array("duminică","luni","marți","miercuri","joi","vineri","sâmbătă"),
+		'today' => "astăzi",
+		'yesterday' => "ieri",
+		'tomorrow' => "mâine",
+		'f_past' => "Întarziat",
+		'f_today' => "Astăzi și mâine",
+		'f_soon' => "În curand",
+		'action_edit' => "Modificare",
+		'action_note' => "Modifică notiță",
+		'action_delete' => "Șterge",
+		'action_priority' => "Prioritate",
+		'action_move' => "Mută la",
+		'notes' => "Notițe:",
+		'notes_show' => "Afișează",
+		'notes_hide' => "Ascunde",
+		'list_new' => "Listă nouă",
+		'list_rename' => "Redenumește listă",
+		'list_delete' => "Șterge listă",
+		'list_publish' => "Publică listă",
+		'list_showcompleted' => "Arată sarcinile efectuate",
+		'list_clearcompleted' => "Șterge sarcinile efectuate",
+		'alltags' => "Toate etichetele:",
+		'alltags_show' => "Arată tot",
+		'alltags_hide' => "Ascunde tot",
+		'a_settings' => "Setări",
+		'rss_feed' => "Feed RSS",
+		'feed_title' => "%s",
+		'feed_description' => "Sarcini noi în %s",
+
+		/* Settings */
+		'set_header' => "Setări",
+		'set_title' => "Titlu",
+		'set_title_descr' => "(specifică dacă dorești să schimbi titlul standard)",
+		'set_language' => "Limbă",
+		'set_protection' => "Protecție cu parolă",
+		'set_enabled' => "Activat",
+		'set_disabled' => "Dezactivat",
+		'set_newpass' => "Parolă nouă",
+		'set_newpass_descr' => "(lasă necompletat dacă nu schimbi parola curentă)",
+		'set_smartsyntax' => "Sintaxă deșteaptă",
+		'set_smartsyntax_descr' => "(/prioritate/ sarcină /etichete/)",
+		'set_autotz' => "Fus orar automat",
+		'set_autotz_descr' => "(determină compensarea fusului orar cu javascript)",
+		'set_autotag' => "Etichetare automată",
+		'set_autotag_descr' => "(adaugarea automată a etichetei filtrului de etichete curent la sarcina nou creată)",
+		'set_sessions' => "Metoda de manipulare a sesiunilor",
+		'set_sessions_php' => "PHP",
+		'set_sessions_files' => "Fișiere",
+		'set_firstdayofweek' => "Prima zi a săptămânii",
+		'set_duedate' => "Formatul calendarului de sarcini întârziate",
+		'set_date' => "Formatul datei",
+		'set_shortdate' => "Formatul scurt al datei",
+		'set_clock' => "Formatul orei",
+		'set_12hour' => "12-ore",
+		'set_24hour' => "24-ore",
+		'set_submit' => "Salvează modificările",
+		'set_cancel' => "Anulează",
+		'set_showdate' => "Arată data sarcinii în listă",
+	);
+}
+
+?>
