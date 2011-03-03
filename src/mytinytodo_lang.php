0a9955d2a44f035b1216e450eb2b936b59525543
Max Pozdeev <maxpozdeev@gmail.com>
2011-03-03 17:18:13 +0300

    * charset header for lang js

--- src/mytinytodo_lang.php
+++ src/mytinytodo_lang.php
@@ -6,7 +6,7 @@ require_once(MTTPATH. 'db/config.php');
 require_once(MTTPATH. 'lang/class.default.php');
 require_once(MTTPATH. 'lang/'. $config['lang']. '.php');
 
-header('Content-type: text/javascript');
+header('Content-type: text/javascript; charset=utf-8');
 echo "mytinytodo.lang.init(". Lang::instance()->makeJS() .");";
 
 ?>
