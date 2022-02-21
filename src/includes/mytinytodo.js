maxpozdeev/mytinytodo

commit 57bd7116c1af11deaa50cc2f3f97daec97f73c69

* use SVG icons as CSS masks to draw action buttons

src/includes/mytinytodo.js
@@ -708,13 +708,13 @@ var mytinytodo = window.mytinytodo = _mtt = {
 						tabLists._alltasks = item;
 						ti += '<li id="list_all" class="mtt-tab mtt-tabs-alltasks'+(item.hidden?' mtt-tabs-hidden':'')+'">'+
 							'<a href="'+_mtt.urlForList(item)+'" title="'+item.name+'"><span>'+item.name+'</span>'+
-							'<div class="list-action"></div></a></li>';
+							'<div class="list-action mtt-img-button"><span></span></div></a></li>';
 					}
 					else {
 						tabLists.add(item);
 						ti += '<li id="list_'+item.id+'" class="mtt-tab'+(item.hidden?' mtt-tabs-hidden':'')+'">'+
 							'<a href="'+_mtt.urlForList(item)+'" title="'+item.name+'"><span>'+item.name+'</span>'+
-							'<div class="list-action"></div></a></li>';
+							'<div class="list-action mtt-img-button"><span></span></div></a></li>';
 					}
 				});
 			}

src/content/themes/default/images/arrdown-hover.svg
@@ -1,5 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 16 16">
- <!--<rect x="0" y="0" width="16" height="16" fill="red"/>-->
- <rect x="1" y="2" width="14" height="12" rx="3" fill="#999999"/>
- <polygon points="4 6, 12 6, 8 10" fill="#ffffff"/>
-</svg>

src/content/themes/default/images/arrdown.svg
@@ -1,3 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 16 16">
-  <polygon points="4 6, 12 6, 8 10" fill="#888888"/>
-</svg>

src/content/themes/default/images/close.svg
@@ -1,6 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-<g stroke-width="1.5" fill="none">
-<path d="M12.0 12.0l-8-8" stroke="#7f7f7f"/>
-<path d="M4.0 12.0l+8-8" stroke="#7f7f7f"/>
-</g>
-</svg>

src/content/themes/default/images/newtask-ext.svg
@@ -1,6 +1,6 @@
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" >
 <!--	<rect x="0" y="0" width="16" height="16" fill="lightpink" /> -->
-<g stroke="#7f7f7f" fill="none">
+<g stroke="#000" fill="none">
 	<polyline points="10.5 1, 1 1, 1 15, 13 15, 13 7.5" stroke-width="1.0" stroke-linejoin="round"/>
 	<g stroke-width="1.0" >
 		<polyline points="7 7, 6 10, 9 9, 15 3, 13 1, 7 7, 6 10" />

src/content/themes/default/images/plus.svg
@@ -1,4 +1,4 @@
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
- <line x1="3" y1="8" x2="13" y2="8" stroke-width="2" stroke="#333" />
- <line x1="8" y1="3" x2="8" y2="13" stroke-width="2" stroke="#333" />
+ <line x1="2" y1="8" x2="14" y2="8" stroke-width="1.5" stroke="#000" />
+ <line x1="8" y1="2" x2="8" y2="14" stroke-width="1.5" stroke="#000" />
 </svg>

src/content/themes/default/images/search-cancel.svg
@@ -1,7 +1,10 @@
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+  <path d="m8 1a7 7 0 0 0-7 7 7 7 0 0 0 7 7 7 7 0 0 0 7-7 7 7 0 0 0-7-7zm-2.4688 3.4688 2.4688 2.4688 2.4688-2.4688 1.0625 1.0625-2.4688 2.4688 2.4688 2.4688-1.0625 1.0625-2.4688-2.4688-2.4688 2.4688-1.0625-1.0625 2.4688-2.4688-2.4688-2.4688 1.0625-1.0625z" fill="#000"/>
+</svg>
+<!-- source:
 <g stroke-width="1.5" fill="none">
 <circle cx="8" cy="8" r="7" fill="#000"/>
 <path d="M11.0 11.0l-6-6" stroke="#ffffff"/>
 <path d="M5.0 11.0l+6-6" stroke="#ffffff"/>
 </g>
-</svg>
+-->

src/content/themes/default/images/search.svg
@@ -1,5 +1,5 @@
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-<g stroke-width="1.5" stroke="#7f7f7f" fill="none">
+<g stroke-width="1.5" stroke="#000" fill="none">
 <path d="M14 14l-4-4"/>
 <circle cx="7" cy="7" r="4.5"/>
 </g>

commit fcf28501e774f963f77e4e59fd0922cb71dc67a3

fix incorrect tab html of newly created list (broken in 57bd711)

src/includes/mytinytodo.js
@@ -713,18 +713,14 @@ var mytinytodo = window.mytinytodo = _mtt = {
 				// open all tasks tab
 				if(_mtt.options.openList == -1) openListId = -1;
 
-				$.each(res.list, function(i,item) {
-					if ( item.id == -1) {
+				$.each(res.list, function(i, item) {
+					if ( item.id == -1 ) {
 						tabLists._alltasks = item;
-						ti += '<li id="list_all" class="mtt-tab mtt-tabs-alltasks'+(item.hidden?' mtt-tabs-hidden':'')+'">'+
-							'<a href="'+_mtt.urlForList(item)+'" title="'+item.name+'"><span>'+item.name+'</span>'+
-							'<div class="list-action mtt-img-button"><span></span></div></a></li>';
+						ti += prepareListHtml(item);
 					}
 					else {
 						tabLists.add(item);
-						ti += '<li id="list_'+item.id+'" class="mtt-tab'+(item.hidden?' mtt-tabs-hidden':'')+'">'+
-							'<a href="'+_mtt.urlForList(item)+'" title="'+item.name+'"><span>'+item.name+'</span>'+
-							'<div class="list-action mtt-img-button"><span></span></div></a></li>';
+						ti += prepareListHtml(item);
 					}
 				});
 			}
@@ -954,13 +950,13 @@ function addList()
 			var item = json.list[0];
 			var i = tabLists.length();
 			tabLists.add(item);
-			if(i > 0) {
-				$('#lists ul').append('<li id="list_'+item.id+'" class="mtt-tab">'+
-						'<a href="#" title="'+item.name+'"><span>'+item.name+'</span>'+
-						'<div class="list-action"></div></a></li>');
+			if (i > 0) {
+				$('#lists ul').append(prepareListHtml(item));
 				mytinytodo.doAction('listAdded', item);
 			}
-			else _mtt.loadLists();
+			else {
+				_mtt.loadLists();
+			}
 		});
 	});
 };
@@ -1047,6 +1043,19 @@ function loadTasks(opts)
 	});
 };
 
+function prepareListHtml(list)
+{
+	var opentag = '';
+	if (list.id == -1) {
+		opentag = '<li id="list_all" class="mtt-tab mtt-tabs-alltasks' + (list.hidden ? ' mtt-tabs-hidden' : '') + '">';
+	}
+	else {
+		opentag = '<li id="list_' + list.id + '" class="mtt-tab' + (list.hidden ? ' mtt-tabs-hidden' : '') + '">';
+	}
+	return opentag +
+		   '<a href="' + _mtt.urlForList(list) + '" title="' + list.name + '"><span>' + list.name + '</span>' +
+		   '<div class="list-action mtt-img-button"><span></span></div></a></li>';
+}
 
 function prepareTaskStr(item, noteExp)
 {