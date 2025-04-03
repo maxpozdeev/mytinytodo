a363de405d731aea7cdd31179583a9aa7038b879
maxpozdeev <maxpozdeev@gmail.com>
2025-04-04 01:26:38 +0300

    fix: extensions were not loaded due to non-list storage
    (cherry picked from commit 42baf175aace3eae5f40de1aeacda1d4ba55a417)

--- src/settings.php
+++ src/settings.php
@@ -92,12 +92,12 @@ else if (isset($_POST['activate']))
                 logAndDie($e->getMessage());
             }
         }
-        else $a = array_diff($a, [$ext]);
+        else $a = array_values(array_diff($a, [$ext]));
         Config::set('extensions', $a);
         Config::save();
     }
     else if (!$activate && in_array($ext, $a)) {
-        $a = array_diff($a, [$ext]);
+        $a = array_values(array_diff($a, [$ext]));
         Config::set('extensions', $a);
         Config::save();
     }
